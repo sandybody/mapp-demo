3a7178bc80aa2d38e3dbd9317f229cc4e0598844
zhangjuntao <625548964@qq.com>
2022-03-01 21:13:04 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Fastclick from 'fastclick'
+
+//方案一
+Fastclick.prototype.focus = function (targetElement) {
+  let length;
+  if (targetElement.setSelectionRange && targetElement.type.indexOf('date') !== 0 && targetElement.type !== 'time' && targetElement.type !== 'month') {
+      length = targetElement.value.length;
+      targetElement.focus();
+      targetElement.setSelectionRange(length, length);
+  } else {
+      targetElement.focus();
+  }
+};
+
+Fastclick.attach(document.body);
+// 方案二
+// if ("addEventListener" in document && "ontouchstart" in window) {
+//   FastClick.prototype.focus = function(targetElement) {
+//     targetElement.focus();
+//   };
+//   document.addEventListener(
+//     "DOMContentLoaded",
+//     function() {
+//       FastClick.attach(document.body);
+//     },
+//     false
+//   );
+// }
 
 Vue.config.productionTip = false
+import './styles/index.scss'
 
 new Vue({
   render: h => h(App),
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,41 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+const path = require('path');
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+const port = process.env.port || 9090; // dev port
+
+module.exports = ({
+  publicPath: './',
+  assetsDir: 'static',
+  configureWebpack: {
+    name: 'mapp-demo',
+    devtool: 'source-map'
+  },
+  lintOnSave: true,
+  productionSourceMap: false,
+  open: true,
+  https: false,
+  hotOnly: false,
+  chainWebpack:(config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('@s', resolve('src/styles'))
+      .set('@a', resolve('src/assets'))
+      .set('@c', resolve('src/components'));
+    const oneOfsMap = config.module.rule('scss').oneOfs.store;
+    oneOfsMap.forEach(item => {
+      item
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
+        .options({
+          // Provide path to the file with resources
+          // 要公用的scss的路径
+          resources: './src/styles/index.scss'
+        })
+        .end();
+    });
+  },
+  
 })
